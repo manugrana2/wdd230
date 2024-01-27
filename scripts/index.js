394502fd46842f68889eceab15e90d5800da7fa0
Jose Manuel Granados <manugrana2@gmail.com>
2024-01-27 17:48:20 -0500

    Added responsive menu to course home page

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+document.querySelector("#menu").addEventListener("click", ()=>{
+   document.querySelector("nav").classList.toggle('close');
+   document.querySelector("#menu").classList.toggle('close');
+})
